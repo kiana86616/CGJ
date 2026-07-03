8d74cf251c2da1441061c81d38af64a476f61775
kiapr <1798422561@qq.com>
2026-07-03 23:53:55 +0800

    Update player.cs

--- Player/player.cs
+++ Player/player.cs
@@ -1,44 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [Header("Anchor Chain")]
+    [SerializeField] private GameObject anchorChainPrefab;
+    [SerializeField] private float anchorChainMaxLength = 100f;
+    [SerializeField] private float mixTime = 0.15f;
+    [SerializeField] private float anchorLandOffset = 0.2f;
 
     private PlayerStateMachine stateMachine;
-    // 公开组件引用，供状态类使用
-    public Rigidbody2D rb{ get; private set;}
-    public CapsuleCollider2D cap{ get; private set;}
-    public Animator anim{ get; private set;}
+    private float lastLaunchTime;
+
+    public Rigidbody2D rb { get; private set; }
+    public CapsuleCollider2D cap { get; private set; }
+    public Animator anim { get; private set; }
+    public float AnchorChainMaxLength => anchorChainMaxLength;
+    public float MixTime => mixTime;
+    public float AnchorLandOffset => anchorLandOffset;
+    public AnchorChain ActiveChain { get; private set; }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cap = GetComponent<CapsuleCollider2D>();
         anim = GetComponentInChildren<Animator>();
 
-        // 初始化状态机，默认进入静止状态
         stateMachine = new PlayerStateMachine(this);
         stateMachine.Initialize(new PlayerIdleState(stateMachine, this));
     }
 
-    void Update()
+    void Update() => stateMachine?.Update();
+    void FixedUpdate() => stateMachine?.FixedUpdate();
+
+    public bool CanLaunchAnchor() => Time.time >= lastLaunchTime + mixTime;
+
+    public AnchorChain SpawnChain(Vector2 direction)
     {
-        stateMachine?.Update();
+        if (!CanLaunchAnchor() || anchorChainPrefab == null)
+            return null;
+
+        if (ActiveChain != null)
+            Destroy(ActiveChain.gameObject);
+
+        Vector2 origin = transform.position;
+        GameObject chainObject = Instantiate(anchorChainPrefab, origin, Quaternion.identity);
+        AnchorChain chain = chainObject.GetComponent<AnchorChain>();
+        if (chain == null)
+            chain = chainObject.AddComponent<AnchorChain>();
+
+        chain.Launch(origin, direction, this, anchorChainMaxLength);
+        ActiveChain = chain;
+        lastLaunchTime = Time.time;
+        return chain;
     }
 
-    void FixedUpdate()
+    public void ClearActiveChain()
     {
-        stateMachine?.FixedUpdate();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        if (ActiveChain == null)
+            return;
+
+        Destroy(ActiveChain.gameObject);
+        ActiveChain = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartAnchorPull(Vector2 anchorPosition, Collider2D platformCollider)
     {
-        
+        stateMachine.ChangeState(new PlayerAnchorPullState(stateMachine, this, anchorPosition, platformCollider));
     }
-}
 }
