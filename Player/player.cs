8abda19eb366e4589765df514a0de84d34ed2092
Sqsezx <s12664@outlook.com>
2026-07-03 18:57:30 +0800

    Update player.cs

--- Player/player.cs
+++ Player/player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private Animator anim;
+    private PlayerStateMachine stateMachine;
+
+    // 公开组件引用，供状态类使用
+    public Rigidbody2D Rb => rb;
+    public Animator Anim => anim;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        // 初始化状态机，默认进入静止状态
+        stateMachine = new PlayerStateMachine(this);
+        stateMachine.Initialize(new PlayerIdleState(stateMachine, this));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        stateMachine?.Update();
+    }
+
+    void FixedUpdate()
+    {
+        stateMachine?.FixedUpdate();
     }
 }
