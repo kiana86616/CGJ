f3f7c7b48c9986c25c3aeda00151f5ea39ec93f9
kiapr <1798422561@qq.com>
2026-07-03 19:44:04 +0800

    Update player.cs

--- Player/player.cs
+++ Player/player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private PlayerStateMachine stateMachine;
     // 公开组件引用，供状态类使用
     public Rigidbody2D rb{ get; private set;}
@@ -27,5 +28,17 @@ public class player : MonoBehaviour
     void FixedUpdate()
     {
         stateMachine?.FixedUpdate();
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+>>>>>>> Stashed changes
     }
 }
